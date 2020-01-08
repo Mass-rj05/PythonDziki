2387f323e0e25fefc55363c74fe5fb630f5fcf7c
Maciej Korcz <maciejkorcz1@gmail.com>
2020-01-08 23:36:34 +0100

    Funcion to make df with names of areas - ready

--- dataCollecting.py
+++ dataCollecting.py
@@ -3,23 +3,50 @@ import re
 import requests
 import matplotlib
 import matplotlib.pyplot as plt
+import soup as soup
 from bs4 import BeautifulSoup
 import ssl
 
 
-url = "https://www.skiresort.info/best-ski-resorts/poland/"
-print(url)
-getpage = requests.get(url)
-getpage_soup = BeautifulSoup(getpage.text, 'html.parser')
-resort_urls = getpage_soup.findAll('a', {'class':'h3'})
+urls = ["https://www.skiresort.info/best-ski-resorts/poland/", "https://www.skiresort.info/best-ski-resorts/austria/", "https://www.skiresort.info/best-ski-resorts/germany/","https://www.skiresort.info/best-ski-resorts/united-kingdom/"]
 
-print(type(resort_urls))
-resort_urls = [i.text for i in resort_urls]
-df = pd.Series(resort_urls)
-df.to_frame()
-print(df.at[49])
-df = df.drop(df[df.index > 49].index)
-df = df.str.replace('\d+.', '')
-df = df.str.replace(' ', '_')
-df = df.str.replace('-', '')
+df = pd.DataFrame()
+
+def makeDFWithAreas(url):
+    getpage = requests.get(url)
+    getpage_soup = BeautifulSoup(getpage.text, 'html.parser')
+    resort_urls = getpage_soup.findAll('a', {'class':'h3'})
+    #print(type(resort_urls))
+    resort_urls = [i.text for i in resort_urls]
+    df = pd.Series(resort_urls)
+    df.to_frame()
+    #print(df.at[49])
+    df = df.drop(df[df.index > 49].index)
+    df = df.str.replace('\d+.\s', '')
+    df = df.str.replace('\s$', '')
+    df = df.str.replace('  ', '')
+    df = df.str.replace('/', '')
+    df = df.str.replace(' ', '-')
+    df = df.str.replace('–-', '')
+    df = df.str.replace('(', '')
+    df = df.str.replace(')', '')
+    return df
+
+
+print(type(makeDFWithAreas(urls[1])))
+
+for site in urls:
+    print(site)
+    df.append(makeDFWithAreas(site), ignore_index= True)
+
 print(df)
+
+
+
+
+
+
+
+
+
+
