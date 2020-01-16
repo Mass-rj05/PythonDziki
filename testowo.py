1b32bab3e7d09e1cc32598da59cd48b9ba8008cf
Mass-rj05 <krzysztof.mirek.mass@gmail.com>
2020-01-16 20:00:36 +0100

    testowo.py

---
+++ testowo.py
@@ -0,0 +1,21 @@
+import pandas as pd
+urls = ["https://www.skiresort.info/best-ski-resorts/poland/", "https://www.skiresort.info/best-ski-resorts/germany/", "https://www.skiresort.info/best-ski-resorts/austria/" ,"https://www.skiresort.info/best-ski-resorts/italy/"]
+lenghtURLS = len(urls)
+y=0
+countryTable=[]
+
+for x in range(lenghtURLS):
+    first = urls[x].split('https://www.skiresort.info/best-ski-resorts/')
+    select = str(first[1])
+    final = select.replace('/', '')
+    if(len(countryTable) == 0):
+        countryTable.append(final)
+    else:
+        if(final != countryTable[y]):
+            countryTable.append(final)
+            y = y + 1
+
+
+print(countryTable)
+
+#Stworzenie listy krajów na podstawie wprowadzonych linków)
