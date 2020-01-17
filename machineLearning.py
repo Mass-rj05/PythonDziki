d15cdbdc905234d88502b756032cadfb615177a5
Mass-rj05 <krzysztof.mirek.mass@gmail.com>
2020-01-17 18:04:15 +0100

    MoÅliwosc sprawdzania stokow wprwoadzajac dane, walidacja itp

--- machineLearning.py
+++ machineLearning.py
@@ -4,9 +4,16 @@ from pandas import DataFrame
 from sklearn import tree
 from matplotlib import pyplot
 from pandas.plotting import scatter_matrix
+import re
+
 #Wczytanie danych z pliku
-df = pd.read_csv (r'export_dataframe.csv')   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-tempTable =df
+def readCSV(path):
+    df = pd.read_csv(path)   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
+    return df
+
+pathTOCSV = 'exportDataframe.csv'
+df = readCSV(pathTOCSV)
+tempTable = df
 #print(df)
 #Uogólnienie danych
 orczyki = df['Chairlift']+df['T-bar']+df['Rope']
@@ -31,12 +38,66 @@ for x in range(lenght):
     else:
         labels.append(0)
 
-scatter_matrix(df)
-pyplot.show()
-
+scatter_matrix(tempTable)
+#pyplot.show()
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(tempTable, labels)
+###
+class checkData: #sprawdzanie poprawności wprowadzonych danych,normalizacjam konwersja danych
+    def __init__(self, data):
+        self.data = data
 
-print(clf.predict([[10, 600, 5, 4,10,1],[60, 1000, 19, 10, 9,2]]))
+    def getData(self): #sprawdzanie poprawności danych ( można użyć white-space , - .
+        regex = "\d{2,5}[\s,-\.]\d{2,5}[\s,-\.]\d{2,5}[\s,-\.]\d{2,5}[\s,-\.]\d{2,5}[\s,-\.]\d{2,5}"
+        if re.search(regex, self.data):
+            match = re.search(regex, self.data)
+            return self.check() #jeśli nie jest w takiej samej formie co lista to check()
+        else:
+            return ("Podałeś błędne dane")
 
+    def check(self): #konwersja
+        regex = "\d{2,5},\d{2,5},\d{2,5},\d{2,5},\d{2,5},\d{2,5}"
+        if re.search(regex, self.data):
+            match = re.search(regex, self.data)
+            return self.makeTable(self.data)
+        else:
+            print(("Konwersja danych... \n"))
+            dict = [' ','-','.']
+            df = self.data
+            df = df.replace(' ', ',')
+            df = df.replace('.', ',')
+            df = df.replace('-', ',')
+            return (self.makeTable(df))
+
+    def makeTable(self, final): #tworzy tabele
+        table = []
+        setting = final.split(',')
+        for x in setting:
+            table.append(x)
+            #Zamiana na inta
+        for i in range(len(table)):
+            table[i] = int(table[i])
+        if(table[2] == (table[3]+table[4]+table[5])):
+            return table
+        else:
+            return ("Podałeś błędne dane")
+
+class InsertDataToCheck:
+    def __init__(self, count):
+        self.count = int(count)
+
+    def doit(self):
+        yourData = []
+        count = self.count
+        print(count)
+
+dane = checkData(input(
+    "Enter the slope parameters  \n Routes total, Elevation difference, Lifts total, Aerial, Circulating, Chairlift: \n"))
+collectData= (dane.getData())
+answer = clf.predict([collectData])
+if(answer == 1):
+    print('Your slope is great ')
+else:
+    print('Your slope is not great ')
+print(collectData)
